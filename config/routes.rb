8da5a4d2b20b192a041d23a286de3d67ea02853d
mengrui song <summerrock1017@gmail.com>
2022-08-09 16:57:16 +0900

    added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Create
+  get '/articles/new', to: 'articles#new', as: :new_article
+  post '/articles', to: 'articles#create', as: :articles
+
+  # Read
+  get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show', as: :article
+
+  # Update
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+
+  # Destroy
+  delete 'articles/:id', to: 'articles#destroy'
 end
